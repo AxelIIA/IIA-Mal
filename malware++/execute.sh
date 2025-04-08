d058c9e1f7947d3fbeba4af0c738c854398b70c0
AxelIIA <AxelIIA@axel.neau-paye@iia-formation.fr>
2025-04-08 20:27:04 +0200

    ajout malware amélioré et anti-malware

---
+++ malware++/execute.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# URL du GIF
+GIF_URL="https://media1.tenor.com/m/x8v1oNUOmg4AAAAd/rickroll-roll.gif" 
+
+# Ouvrir l'URL du GIF dans le navigateur par défaut
+xdg-open "$GIF_URL"
+
+exit 0
